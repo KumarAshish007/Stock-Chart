b490713a7d4dd49a44bc4471bb4d2660ca4ddd44
KumarAshish007 <110601843+KumarAshish007@users.noreply.github.com>
2022-08-25 16:27:04 +0530

    Add files via upload

---
+++ src/urls/Url.js
@@ -0,0 +1,7 @@
+const url = {
+  SERVER_URL: "https://backendtrading.herokuapp.com/",
+};
+export default url;
+//http://localhost:5001/
+// https://aqueous-lowlands-22352.herokuapp.com
+//https://backendtrading.herokuapp.com/history/TCS
